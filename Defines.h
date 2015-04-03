13a2448ebec92edfb68b6e645fbba19382c4b230
20930496 <20930496@ecm-csse2318-l.uniwa.uwa.edu.au>
2015-04-03 12:39:57 +0800

    Initial commit. Max flow implemented.

---
+++ Defines.h
@@ -0,0 +1,23 @@
+#ifndef DEFINES_INCLUDED
+#define DEFINES_INCLUDED
+
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<climits>
+#include<algorithm>
+#include<tuple>
+#include<cmath>
+using namespace std;
+
+typedef long long ll;
+
+typedef tuple<double, double> vec2;
+
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+#define _ ignore
+
+#endif
